76a1b554483a337d01fa320a3465950270b8f922
ginpajac <ginpajac@espol.edu.ec>
2019-11-19 21:59:22 -0500

    Desaparecen pickups, texto

--- Trabajo/Assets/Scripts/PlayerController.cs
+++ Trabajo/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    public Text countText;
+    public Text wintext;
     public float speed;
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        wintext.text = "";
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -19,6 +27,25 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(moveHorizontal,moveVertical);
         rb2d.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            wintext.text = "Ganaste!";
+        }
     }
 }
  
